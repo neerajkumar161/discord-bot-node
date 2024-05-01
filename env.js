d0cdb50deddd1db09cb9cc7b0060c96d86668d3f
Neeraj Kumar <50705472+neerajkumar161@users.noreply.github.com>
2024-05-01 11:24:32 +0530

    create env.js

---
+++ env.js
@@ -0,0 +1,7 @@
+export const TOKENS = {
+  CLIENT_ID: '1234567890123456789', // Application Id
+  CHANNEL_ID: '1234567890123456789', 
+  SERVER_ID: '1234567890123456789',  // [Using guildId in the deploy-commands.js file]
+  BOT_TOKEN: 'MdisdyjZUzzNTAxODYwNDU3NA.GzFQQk.zpbRukCh6vxUL6_-kqP6CAgIWEQ_W-bjE7aVV8',
+  TENOR_KEY: 'BIzaSyBnBzKQI9pgo53_bRbF3kTf0r1HJiNU8sA',  // Generate TENOR key to generate gifs
+};
